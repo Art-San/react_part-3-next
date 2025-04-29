cabff0a7d7e635c61af944eeac61b5badf42ffed
Art San <artsan.mcc@gmail.com>
2025-04-29 16:36:20 +0700

    ок

---
+++ src/app/docs/page.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react'
+
+const page: FC = () => {
+  return <div>Главный раздел докс</div>
+}
+
+export default page
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,25 +3,24 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div>
-      <Link className="text-blue-800" href="/about">
-        Перейти на страницу о нас
-      </Link>
-      <br />
-      <Link className="text-red-600" href="/dashboard">
-        Перейти в панель управления
-      </Link>
+      <h1>Главная страница</h1>
+      <nav>
+        <h2>Документация:</h2>
+        <ul>
+          <li>
+            <Link href="/docs">Общий</Link>
+          </li>
+          <li>
+            <Link href="/docs/React">React</Link>
+          </li>
+          <li>
+            <Link href="/docs/React/Hooks">React Hooks</Link>
+          </li>
+          <li>
+            <Link href="/docs/React/Hooks/useState">Hook useState</Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   )
 }
-// 'use client'
-// import { useState } from 'react'
-
-// export default function Home() {
-//   const [count, setCount] = useState(0)
-//   return (
-//     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-//       <div className="">{count}</div>
-//       <button onClick={() => setCount((prev: number) => prev + 1)}>жми</button>
-//     </div>
-//   )
-// }
