d49bf5f9cd7844eae6b05ea78393992f3ebb58aa
Art San <artsan.mcc@gmail.com>
2025-05-01 00:25:55 +0700

    ok

---
+++ src/app/contacts/page.tsx
@@ -0,0 +1,15 @@
+export const metadata = {
+  title: {
+    absolute: '✅: 200-891'
+  },
+  description: 'Нас можно найти...'
+}
+
+export default function Contacts() {
+  return (
+    <div>
+      <p>Contacts</p>
+      <p>+1234567890</p>
+    </div>
+  )
+}
