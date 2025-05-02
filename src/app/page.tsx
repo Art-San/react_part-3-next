bde46c499fd9661cb82fee42b18dd7966cc6b125
Art San <artsan.mcc@gmail.com>
2025-05-02 15:33:18 +0700

    ok

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,16 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <>
-      <h1>Главная страница</h1>
-      <Link href="about">На страницу о компании</Link>
-      <br />
-      <Link href="contacts" replace>
-        Наши контакты
+    <div>
+      <Link href="/articles/news?language=ru&edit=true">
+        Читать по-русски (с модалкой)
       </Link>
-    </>
+      <br />
+      <Link href="/articles/news?language=en">Read in English</Link>
+      <br />
+      <Link href="/articles/news?language=fr">Lire en francais</Link>
+      <br />
+      <Link href="/articles/news?language=de">Auf deutsch lesen</Link>
+    </div>
   )
 }
