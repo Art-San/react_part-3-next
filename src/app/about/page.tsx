d0330f565e98e83cd6124dbe9094df1eb11c41e4
Art San <artsan.mcc@gmail.com>
2025-06-03 21:04:31 +0700

    ok

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,10 @@
-export default function About() {
+import { cookies } from 'next/headers'
+
+export default async function About() {
+  const cookiesStore = await cookies()
+  const theme = cookiesStore.get('theme')
+  console.log(12, theme)
+
   return (
     <div>
       <h1>О компании</h1>
