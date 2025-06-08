8ca48d55d7b6059404e89415a8f42882464f8c82
Art San <artsan.mcc@gmail.com>
2025-06-08 23:30:57 +0700

    ok

--- src/app/client-route/page.tsx
+++ src/app/client-route/page.tsx
@@ -4,27 +4,35 @@ import React from 'react'
 import Slider from 'react-slick'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
+import { useTheme } from '@/components/theme-provider'
 
 export default function ClientRoutePage() {
+  const theme = useTheme()
   const settings = {
     dots: true
   }
   return (
     <div className="image-slider-container">
-      <Slider {...settings}>
-        <div>
-          <img src="https://s1.1zoom.me/prev/608/Mountains_Lake_Lake_Sary-Chelek_Kyrgyzstan_607774_600x400.jpg" />
-        </div>
-        <div>
-          <img src="https://s1.1zoom.me/prev/608/Mountains_Lake_Lake_Sary-Chelek_Kyrgyzstan_607774_600x400.jpg" />
-        </div>
-        <div>
-          <img src="https://s1.1zoom.me/prev/608/Mountains_Lake_Lake_Sary-Chelek_Kyrgyzstan_607774_600x400.jpg" />
-        </div>
-        <div>
-          <img src="https://s1.1zoom.me/prev/608/Mountains_Lake_Lake_Sary-Chelek_Kyrgyzstan_607774_600x400.jpg" />
-        </div>
-      </Slider>
+      <div className="">
+        <Slider {...settings} className="">
+          <div>
+            <img src="https://s1.1zoom.me/prev/608/Mountains_Lake_Lake_Sary-Chelek_Kyrgyzstan_607774_600x400.jpg" />
+          </div>
+          <div>
+            <img src="https://s1.1zoom.me/prev/608/Mountains_Lake_Lake_Sary-Chelek_Kyrgyzstan_607774_600x400.jpg" />
+          </div>
+          <div>
+            <img src="https://s1.1zoom.me/prev/608/Mountains_Lake_Lake_Sary-Chelek_Kyrgyzstan_607774_600x400.jpg" />
+          </div>
+          <div>
+            <img src="https://s1.1zoom.me/prev/608/Mountains_Lake_Lake_Sary-Chelek_Kyrgyzstan_607774_600x400.jpg" />
+          </div>
+        </Slider>
+      </div>
+
+      <div className=" mt-5" style={{ background: theme.color.primary }}>
+        Теиа применяется
+      </div>
     </div>
   )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
+import { ThemeProvider } from '@/components/theme-provider'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -27,12 +28,13 @@ export default async function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head></head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased max-h-screen flex flex-col bg-gray-50 dark:bg-gray-800 dark:text-white`}
-      >
-        <main className="p-8">{children}</main>
-      </body>
+      <ThemeProvider>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased max-h-screen flex flex-col bg-gray-50 dark:bg-gray-800 dark:text-white`}
+        >
+          <main className="p-8">{children}</main>
+        </body>
+      </ThemeProvider>
     </html>
   )
 }
