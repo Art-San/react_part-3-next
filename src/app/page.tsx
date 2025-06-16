208fdfe031c1dfd3ea482c06cda0994fbcfadb54
Art San <artsan.mcc@gmail.com>
2025-06-16 17:41:13 +0700

    ok

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+import Link from 'next/link'
+
 export default function Home() {
   return (
     <>
-      <main>Главная</main>
+      <main>
+        <div className="">
+          <h1>Главная</h1>
+          <Link href={'/products-db'}>
+            <p>Страница с продуктами</p>
+          </Link>
+        </div>
+      </main>
     </>
   )
 }
--- src/app/products-db/[id]/product-edit-form.tsx
+++ src/app/products-db/[id]/product-edit-form.tsx
@@ -37,53 +37,60 @@ export default function EditProductForm({ product }: { product: Product }) {
   //   );
 
   return (
-    <form action={formAction} className="p-4 space-y-4 max-w-96">
-      {/* <input name="id" value={product.id} /> */}
-      <div>
-        <label className="text-white">
-          Название
-          <input
-            type="text"
-            className="block w-full p-2 text-black border rounded bg-white"
-            name="title"
-            defaultValue={product.title}
-          />
-        </label>
-        {state.errors.title && (
-          <p className="text-red-500">{state.errors.title}</p>
-        )}
-      </div>
-      <div>
-        <label className="text-white">
-          Цена
-          <input
-            type="number"
-            className="block w-full p-2 text-black border rounded bg-white"
-            name="price"
-            defaultValue={product.price}
-          />
-        </label>
-        {state.errors.price && (
-          <p className="text-red-500">{state.errors.price}</p>
-        )}
-      </div>
-      <div>
-        <label className="text-white">
-          Описание
-          <textarea
-            className="block w-full p-2 text-black border rounded bg-white"
-            name="description"
-            defaultValue={product.description ?? ''}
-          />
-        </label>
-        {state.errors.description && (
-          <p className="text-red-500">{state.errors.description}</p>
-        )}
-      </div>
-      <button type="submit" disabled={isPending} className="bg-blue-500">
-        {isPending ? 'Отправляется' : 'Отправить'}
-      </button>
-      {/* <Submit /> */}
-    </form>
+    <>
+      <h1 className=" text-2xl">Редактирование товара</h1>
+      <form action={formAction} className="p-4 space-y-4 max-w-96">
+        {/* <input name="id" value={product.id} /> */}
+        <div>
+          <label className="text-white">
+            Название
+            <input
+              type="text"
+              className="block w-full p-2 text-black border rounded bg-white"
+              name="title"
+              defaultValue={product.title}
+            />
+          </label>
+          {state.errors.title && (
+            <p className="text-red-500">{state.errors.title}</p>
+          )}
+        </div>
+        <div>
+          <label className="text-white">
+            Цена
+            <input
+              type="number"
+              className="block w-full p-2 text-black border rounded bg-white"
+              name="price"
+              defaultValue={product.price}
+            />
+          </label>
+          {state.errors.price && (
+            <p className="text-red-500">{state.errors.price}</p>
+          )}
+        </div>
+        <div>
+          <label className="text-white">
+            Описание
+            <textarea
+              className="block w-full p-2 text-black border rounded bg-white"
+              name="description"
+              defaultValue={product.description ?? ''}
+            />
+          </label>
+          {state.errors.description && (
+            <p className="text-red-500">{state.errors.description}</p>
+          )}
+        </div>
+        <button
+          type="submit"
+          disabled={isPending}
+          className="bg-blue-500 px-2 py-1 text-white"
+        >
+          {isPending ? 'Сохранение' : 'Сохранить изменения'}
+        </button>
+        {/* <Submit /> */}
+      </form>
+    </>
   )
 }
