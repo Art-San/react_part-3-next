f8543e4c0a48baed1e2a8ae97b4fbf69e5f000d0
Art San <artsan.mcc@gmail.com>
2025-06-20 20:13:50 +0700

    ok

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+}
 
-export default nextConfig;
+export default nextConfig
+
+module.exports = {
+  images: {
+    remotePatterns: [
+      new URL('https://static-cse.canva.com/blob/846900/**'),
+      new URL('https://i.pinimg.com/736x/be/39/7c/**')
+    ],
+    formats: ['image/avif', 'image/webp']
+  }
+}
